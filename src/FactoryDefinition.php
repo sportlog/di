76efdfe95e10bf8ad175c5fe9b77a7806f53168d
Johannes Aberidis <jo@e175.net>
2021-12-14 20:25:45 +0100

    add comments

--- src/FactoryDefinition.php
+++ src/FactoryDefinition.php
@@ -13,17 +13,27 @@ namespace Sportlog\DI;
 use Closure;
 
 /**
- * Undocumented class
+ * Defines the Closure and it's dependencies to instantiate a type.
  */
 class FactoryDefinition {
     public function __construct(private Closure $factory, private array $deps = [])
     {
     }
 
+    /**
+     * The factory to create a type.
+     *
+     * @return Closure
+     */
     public function getFactory(): Closure {
         return $this->factory;
     }
 
+    /**
+     * List of class ids to be injected into the Closure.
+     *
+     * @return array
+     */
     public function getDependencies(): array {
         return $this->deps;
     }
